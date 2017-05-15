cbaec307dbad5cbecd55897985248e314a8f33a5
dastangoo <hamed.dastangoo@gmail.com>
2017-05-15 19:53:23 +0430

    Just a commit

--- example01.js
+++ example01.js
@@ -176,6 +176,16 @@ $.extend($.prototype, {
   }),
   children: makeTraverser(function(){
     return this.children;
-  })
+  }),
+  attr: function(attrName, value){
+    if (arguments.length > 1) {
+      $.each(this, function(i, el){
+        el.setAttribute(attrName, value);
+      });
+    }
+    else {
+      return this[0] && this[0].getAttribute(attrName);
+    }
+  }
 
 });
